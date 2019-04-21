4c8814ee66393a03a72fdb55a4dafcc2b075339b
Alex-Fun <408889098@qq.com>
2019-04-21 19:08:12 +0800

    Update run.sh

--- models/research/run.sh
+++ models/research/run.sh
@@ -24,7 +24,7 @@ pipeline_config_path=$output_dir/$config
 # tinymind已经支持引用上一次的运行结果，这一行需要删掉，不然会出现上一次的运行结果被清空的状况。
 # rm -rvf $output_dir/*
 
-protoc object_detection/protos/*.proto --python_out=.
+
 
 # 因为dataset里面的东西是不允许修改的，所以这里要把config文件复制一份到输出目录
 cp $dataset_dir/$config $pipeline_config_path
